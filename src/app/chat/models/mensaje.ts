688667b931788ef2f0d0528f5c711f591418ba69
luis <luis.silva@ids.com.mx>
2022-06-02 14:43:05 -0500

    front-app-chat-sockets

---
+++ src/app/chat/models/mensaje.ts
@@ -0,0 +1,12 @@
+export class Mensaje {
+    texto:string='';
+    fecha:Date|undefined|any;
+    username:string='';
+    tipo:string='';
+    color:string='';
+
+    /* constructor(){
+        this.fecha = new Date().get;
+    }
+ */
+}
